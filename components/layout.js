e0f5abf3f57bdc73772c1a4c313f34789dce5dac
SouthWatermel0n <lzw_roven@outlook.com>
2022-04-22 10:42:05 +0800

    feat: text

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from "./layout.module.css"
 import utilStyles from "../styles/utils.module.css"
 import Link from "next/link"
 
-const name = "[Roven Siren]"
+const name = "[I love Rock&Roll]"
 export const siteTitle = "Next.js Sample Website"
 
 export default function Layout({ children, home }) {
